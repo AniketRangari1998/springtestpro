320b8b4b3d3a08a51021ee0102ffbf8e956f6f6c
aniket <aniketrangari1998@gmail.com>
2022-06-22 11:50:47 +0530

    jenkins commit

---
+++ src/test/java/com/spring/testpro/SpringtestproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.testpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
